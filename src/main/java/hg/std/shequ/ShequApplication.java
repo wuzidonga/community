34a0ee21684e340ac096d022749b19594c3f8364
吴梓东 <1464605738@qq.com>
2020-09-19 21:06:30 +0800

    fist commit

---
+++ src/main/java/hg/std/shequ/ShequApplication.java
@@ -0,0 +1,13 @@
+package hg.std.shequ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShequApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShequApplication.class, args);
+    }
+
+}
